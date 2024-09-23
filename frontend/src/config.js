0d44a52624b7bcdf9fe961c547ac6593a7cad583
Muiz0399 <muhammadmuiz0399@gmail.com>
2024-09-23 18:13:08 +0500

    hope so

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,4 +2,5 @@ const dev = window.location.hostname === "localhost";
 
 export const API_URL = dev
   ? "http://localhost:4001/api" 
-  : "https://blog-app-back-ten.vercel.app"; 
+  : "https://blog-app-api-olive.vercel.app"; 
+  
